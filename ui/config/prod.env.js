198f94b28c9a06a570e735e7f970dad236e557bd
sunface <cto@188.com>
2018-09-17 15:33:32 +0800

    update

--- ui/config/prod.env.js
+++ ui/config/prod.env.js
@@ -3,8 +3,8 @@ module.exports = {
   ENV_CONFIG: '"prod"',
   BASE_API: '"http://47.91.140.160:9091"',
   TFE_ADDR: '"http://47.91.140.160:8081/service/api"',
-  API_GRAFANA: '"http://10.7.29.62:3000/d/1ZLGyLtiz/tfe-api?refresh=5s&orgId=3&var-api_id="',
-  SERVICE_GRAFANA: '"http://10.7.29.62:3000/d/gFZ_ZUtik/tfe-service?refresh=5s&orgId=3&var-service="',
-  LABEL_GRAFANA: '"http://10.7.29.62:3000/d/UXhO2Upik/tfe-label?refresh=5s&orgId=3&var-label="',
+  API_GRAFANA: '"http://47.91.140.160:3000/d/1ZLGyLtiz/tfe-api?refresh=5s&orgId=1&var-api_id="',
+  SERVICE_GRAFANA: '"http://47.91.140.160::3000/d/gFZ_ZUtik/tfe-service?refresh=5s&orgId=1&var-service="',
+  LABEL_GRAFANA: '"http://47.91.140.160::3000/d/UXhO2Upik/tfe-label?refresh=5s&orgId=1&var-label="',
   ENV: '"生产"'
 }
